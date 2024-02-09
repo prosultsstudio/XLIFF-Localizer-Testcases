7d009e3c6ce2fead46fe5d633d2ea562d9cb6d4c
Leen van der Bent <leenvdb@planet.nl>
2024-02-09 07:04:55 +0100

    Initial Commit

---
+++ XLIFF Localizer Testcases/XLIFF_Localizer_TestcasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  XLIFF_Localizer_TestcasesApp.swift
+//  XLIFF Localizer Testcases
+//
+//  Created by Leen van der Bent on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct XLIFF_Localizer_TestcasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
